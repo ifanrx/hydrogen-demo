a831c991b18f604b0f1d82ada7dfc7a0efe89632
Ernest He <ernest.he2009@gmail.com>
2017-06-06 17:26:50 +0800

    update mock data to catch up sdk upgrade

--- hello-world/mock/mock.js
+++ hello-world/mock/mock.js
@@ -10,27 +10,21 @@ module.exports = {
     "objects": [{
       "created_at": 1487053095,
       "id": 7,
-      "response": {
-        recordID: 1,
-        bookName: '傲慢与偏见',
-        isEditing: false,
-      }
+      "recordID": 1,
+      "bookName": "傲慢与偏见",
+      "isEditing": false,
     }, {
       "created_at": 1487053110,
       "id": 7,
-      "response": {
-        recordID: 2,
-        bookName: '双城记',
-        isEditing: false,
-      }
+      "recordID": 2,
+      "bookName": "双城记",
+      "isEditing": false,
     }, {
       "created_at": 1487053110,
       "id": 7,
-      "response": {
-        recordID: 3,
-        bookName: '黑客与画家',
-        isEditing: false,
-      }
+      "recordID": 3,
+      "bookName": "黑客与画家",
+      "isEditing": false,
     }]
   }
 }
