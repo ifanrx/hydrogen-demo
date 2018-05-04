3297a0510071ea73a13b6aff10626342d09dc99d
zero <379338323@qq.com>
2018-05-04 12:06:06 +0800

    添加 calendar-demo 文档

--- calendar-demo/app.js
+++ calendar-demo/app.js
@@ -1,9 +1,11 @@
 //app.js
+const config = require('./config/config.js')
+
 App({
   onLaunch: function () {
     wx.BaaS = requirePlugin('sdkPlugin')
     wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment) //让插件帮助完成登录、支付等功能
-    wx.BaaS.init('849144d9131d00651021')
+    wx.BaaS.init(config.BAAS_CLIENT_ID)
   },
   globalData: {}
 })
---
+++ calendar-demo/config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  BAAS_CLIENT_ID: '', // 通过知晓云管理面板获取 ClientID
+}
