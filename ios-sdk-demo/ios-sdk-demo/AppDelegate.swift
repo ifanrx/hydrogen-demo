08839ac4819d6aa64c2ad76e06982caed7b75ac8
quanhua.peng <pengquanhua@ifanr.com>
2019-10-31 16:44:55 +0800

    更新 MinCloud API

--- ios-sdk-demo/ios-sdk-demo/AppDelegate.swift
+++ ios-sdk-demo/ios-sdk-demo/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        BaaS.register(clientID: "c52d24eed6e042125d3e")
+        BaaS.register(clientID: "ea4f9c7f22af27effc99")
         BaaS.isDebug = true
         return true
     }
