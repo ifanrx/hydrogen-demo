7b752f1fce5bff74828c89068a57db19a3f3131d
quanhua.peng <pengquanhua@ifanr.com>
2020-07-06 19:20:22 +0800

    iOS SwiftUIDemo 增加 AppClip

---
+++ swiftui-demo/BookShelfClip/BookShelf/Book/BookList.swift
@@ -0,0 +1,24 @@
+//
+//  BookList.swift
+//  BookShelf
+//
+//  Created by quanhua on 2019/9/26.
+//  Copyright © 2019 ifanr. All rights reserved.
+//
+
+import SwiftUI
+
+struct BookList: View {
+    @ObservedObject var store = BookStore()
+    
+    var body: some View {
+        List(store.books) { book in
+            NavigationLink(destination: BookDetailView(book: book)) {
+                BookItemRow(book: book)
+            }
+        }
+        .onAppear(perform: { self.store.fetch() })
+        .navigationBarTitle(Text("书籍列表"))
+    }
+}
+
---
+++ swiftui-demo/BookShelfClip/BookShelfClip/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  BookShelfClip
+//
+//  Created by quanhua on 2020/7/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
